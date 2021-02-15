6347692cba75d6c8e1d696bc17c192e692a35292
Talita-8 <tlthmf1@outlook.com>
2021-02-15 17:51:10 -0300

    Alert incluso

--- bq-app/src/routes.js
+++ bq-app/src/routes.js
@@ -1,8 +1,20 @@
 import React from 'react';
-import Home from './Home/Home';
-import Login from './Login/Login';
-import Subscribe from './Subscribe/Subscribe';
-import { Switch, Route } from 'react-router-dom';
+import Home from './Home';
+import Login from './Login';
+import Subscribe from './Subscribe';
+import { Switch, Route, Redirect } from 'react-router-dom';
+
+// const PrivateRoute = ({component: Component, ...rest}) => (
+// <Route 
+//     {...rest}
+//     render={props => ({Login} ? (
+//         <Component {...props} />
+//     ) : (
+//         <Redirect to={{pathname:"/home", state: {from: props.location}}} />
+//     )
+//     )}
+// />
+// )
 
 const Routes = () => {
     return(
