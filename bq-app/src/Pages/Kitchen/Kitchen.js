ddbd818445f66af607b13831f44de1eb8af79d38
Talita-8 <tlthmf1@outlook.com>
2021-03-01 23:15:22 -0300

    Logout Button

--- bq-app/src/Pages/Kitchen/Kitchen.js
+++ bq-app/src/Pages/Kitchen/Kitchen.js
@@ -1,60 +1,38 @@
-import '../App.css';
+import './Kitchen.css';
 import React, { useState, useEffect } from 'react';
+import { requestKitchen } from './functions';
 import Header from '../../Components/Header';
 
 function Kitchen(){
 
   const [orders, setOrders] = useState([]);
   
-  useEffect(() =>{
-    console.log("useffect", orders)
-
-  },[orders])
-
   useEffect(() => {
-
-   const idUser = localStorage.getItem("token");
-    let requestOptions = {
-      method: 'GET',
-      headers: { 
-        'Content-Type': 'application/json',
-        'Authorization': `${idUser}`
-      },
-      redirect: 'follow'
-    };
-
-  fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
-    .then(response => response.json())
-    .then(result => {console.log(result)
-      
-      
-      setOrders(result)
-    })
-    .catch(error => console.log('error', error));
+     requestKitchen(setOrders)
   },[]);
 
- const GetOrders = orders.map((i) => {
-   console.log(i)
-  return(
-    <div key={7467546}>
-      <p>Status {i.status}</p>
-      <p>Mesa {i.table}</p>
-      <p>Cliente {i.client_name}</p>
-      <p>Data/hora {i.createdAt}</p>
-      <p>Produtos {i.Products}</p> 
-    </div>
-  )
-}) 
 
- return(<div>
-   <Header />{
-     GetOrders.sort(function(first, last){
-      if(first.createdAt < last.createdAt)
-       return first
-     })
-   } </div>
-    
- )
- }
+  const GetOrders = orders.map((i) => {
+    return(
+      <div className="orders-pending" key={Math.random()}>
+        <p>Status: {i.status}</p>
+        <p>Mesa: {i.table}</p>
+        <p>Cliente: {i.client_name}</p>
+        <p>Data: {i.createdAt.replace("T", " |  Hora:").split("Z")}</p>
+      </div>
+    )
+  });
+
+return(
+ <div>
+   <Header />
+    <div className="orders-container">{
+      GetOrders.sort(function(first, last){
+        if(first.createdAt < last.createdAt)
+         return first
+      })
+   }</div>
+ </div>
+ ) };
 
 export default Kitchen;
---
+++ bq-app/src/Pages/Kitchen/functions.js
@@ -0,0 +1,20 @@
+
+const requestKitchen = (state) => {
+    const idUser = localStorage.getItem("token");
+    let requestOptions = {
+      method: 'GET',
+      headers: { 
+        'Content-Type': 'application/json',
+        'Authorization': `${idUser}`
+      },
+      redirect: 'follow'
+    };
+
+  fetch("https://lab-api-bq.herokuapp.com/orders", requestOptions)
+    .then(response => response.json())
+    .then(result => {console.log(result)
+      
+      state(result)
+    })
+    .catch(error => console.log('error', error));
+}
